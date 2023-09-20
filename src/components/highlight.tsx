d546555d6f2104944d3971084171f8c484c6b0c8
mytrofanov <mytrofanovmaxym@gmail.com>
2023-09-20 16:28:40 +0300

    feat: highligt

--- src/components/highlight.tsx
+++ src/components/highlight.tsx
@@ -1,21 +1,23 @@
-import { ReactNode, useState } from 'react';
+import React, { ReactNode, useState } from 'react';
 import styles from './highlight.module.css';
 
 interface HighlightProps {
-    text: string;
     searchText: string[];
 }
 
 export const Highlight = (props: HighlightProps) => {
-    const { text, searchText } = props;
+    const { searchText } = props;
+    const [inputText, setInputText] = useState<string>('');
     const [highlightedText, setHighlightedText] = useState<(string | ReactNode)[]>('');
+    const editableDivRef = React.useRef<HTMLDivElement>(null);
+    const textToShow = highlightedText.length > 0 ? highlightedText : inputText;
 
     const createRegex = () => {
         const searchTextRegex = searchText.map(word => `\\b${word}\\b`).join('|');
         return new RegExp(`(${searchTextRegex})`, 'gi');
     };
 
-    const handleScanClick = () => {
+    const highlightAndSet = (text: string) => {
         const parts = text.split(createRegex());
         const highlightedParts = parts.map((part, index) => {
             const isHighlighted = searchText.includes(part.toLowerCase());
@@ -31,10 +33,20 @@ export const Highlight = (props: HighlightProps) => {
         setHighlightedText(highlightedParts);
     };
 
+    const handleScanClick = () => {
+        if (!editableDivRef.current) return;
+        const editedText = editableDivRef.current.innerText;
+        console.log(editedText);
+        setInputText(editedText);
+        highlightAndSet(editedText);
+    };
+
     return (
         <div>
+            <div contentEditable="true" className={styles.text} ref={editableDivRef}>
+                {textToShow}
+            </div>
             <button onClick={handleScanClick}>Scan</button>
-            <div>{highlightedText && <div>{highlightedText}</div>}</div>
         </div>
     );
 };
