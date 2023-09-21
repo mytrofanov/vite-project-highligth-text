11c54c28c7d2a7fe0e28e98cc89e912324fc9438
mytrofanov <mytrofanovmaxym@gmail.com>
2023-09-21 08:11:21 +0300

    feat: render

--- src/components/highlight.tsx
+++ src/components/highlight.tsx
@@ -5,6 +5,23 @@ interface HighlightProps {
     searchText: string[];
 }
 
+const highlightAndSet = (text: string, searchText: string[], editableDivRef: React.RefObject<HTMLDivElement>) => {
+    const createRegex = () => {
+        const searchTextRegex = searchText.map(word => `\\b${word}\\b`).join('|');
+        return new RegExp(`(${searchTextRegex})`, 'gi');
+    };
+
+    const parts = text.split(createRegex());
+    const highlightedParts = parts.map((part, index) => {
+        const isHighlighted = searchText.includes(part.toLowerCase());
+        return isHighlighted ? `<b key=${index} class=${styles.highlight}>${part}</b>` : part;
+    });
+
+    if (editableDivRef.current) {
+        editableDivRef.current.innerHTML = highlightedParts.join('');
+    }
+};
+
 export const Highlight = (props: HighlightProps) => {
     const { searchText } = props;
     const editableDivRef = React.useRef<HTMLDivElement>(null);
@@ -14,28 +31,12 @@ export const Highlight = (props: HighlightProps) => {
             editableDivRef.current.focus();
         }
     }, []);
-
-    const createRegex = () => {
-        const searchTextRegex = searchText.map(word => `\\b${word}\\b`).join('|');
-        return new RegExp(`(${searchTextRegex})`, 'gi');
-    };
-
-    const highlightAndSet = (text: string) => {
-        const parts = text.split(createRegex());
-        const highlightedParts = parts.map((part, index) => {
-            const isHighlighted = searchText.includes(part.toLowerCase());
-            return isHighlighted ? `<b key=${index} class=${styles.highlight}>${part}</b>` : part;
-        });
-
-        if (editableDivRef.current) {
-            editableDivRef.current.innerHTML = highlightedParts.join('');
-        }
-    };
+    console.log('Highlight render');
 
     const handleScanClick = () => {
         if (!editableDivRef.current) return;
         const editedText = editableDivRef.current.innerHTML;
-        highlightAndSet(editedText);
+        highlightAndSet(editedText, searchText, editableDivRef);
     };
 
     const handleScanClear = () => {
